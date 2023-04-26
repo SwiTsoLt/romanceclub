e7ffa252d9894c3a6aabeda8d79c97e604ecb71b
unknown <8268363@gmail.com>
2023-04-26 10:35:29 +0300

    change character image

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,16 +6,16 @@ import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/api/joke.ts";
 import * as $2 from "./routes/favorites.tsx";
-import * as $2 from "./routes/stats.tsx";
 import * as $3 from "./routes/index.tsx";
+import * as $4 from "./routes/stats.tsx";
 
 const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
     "./routes/api/joke.ts": $1,
     "./routes/favorites.tsx": $2,
-    "./routes/stats.tsx": $2,
     "./routes/index.tsx": $3,
+    "./routes/stats.tsx": $4,
   },
   islands: {},
   baseUrl: import.meta.url,
